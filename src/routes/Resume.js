2387a71dd2c3f3eb88a3edcb428d58c9a217a7d5
Kyle Franklin <kpfranklin22@gmail.com>
2023-03-30 17:07:22 -0400

    editing imports

--- src/routes/Resume.js
+++ src/routes/Resume.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
 import HeroImg2 from '../components/HeroImg2';
 import AboutContent from '../components/AboutContent';
 import pdf from '../assets/sample-resume.pdf';
