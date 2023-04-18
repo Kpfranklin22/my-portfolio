991a3c3e03923cfe3b137d6469ba734d76513336
Kyle Franklin <kpfranklin22@gmail.com>
2023-04-17 20:16:07 -0400

    removed ddbb

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,5 +1,4 @@
 import projectPokedex from '../assets/project-pokedex.png';
-import projectDdBb from '../assets/project-ddbb.png';
 import projectEe from '../assets/project-ee.png';
 import projectFs from '../assets/free-school.png';
 
@@ -17,13 +16,6 @@ const ProjectCardData = [
     text: 'An app for "apps" As someone who LOVES eating, sometimes I get stuck in a rut of eating the same thing all the time and it is nice to have a change of pace. Everybody Eats is a randomized meal generator that includes a youtube link with "how-to" instructions and a recipe to prepare a dish you have potentially never had before!',
     view: 'https://kpfranklin22.github.io/Everybody-Eats/',
     source: 'https://github.com/Kpfranklin22/Everybody-Eats',
-  },
-  {
-    imgsrc: projectDdBb,
-    title: 'Digital DIY Bulletin Board',
-    text: 'This project is an online community bulletin board for local bands in the DIY scene with user authentification functionality! My project partners and I seeded it with spoof event information for a laugh during presentation. Enjoy!',
-    view: 'https://digital-diy-bulletin-board.herokuapp.com/',
-    source: 'https://github.com/Kpfranklin22/digital-diy-bulletin-board',
   },
   {
     imgsrc: projectFs,
